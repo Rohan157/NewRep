d8be32397d0ebf30ba053b3c406ac46895d7be3c
Rohan_157 <73091151+Rohan157@users.noreply.github.com>
2022-07-05 03:18:36 -0700

    FirstCommit

---
+++ WebApplication_API/WebApplication_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+       
+    }
+}
---
+++ WebApplication_API/WebApplication_API/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApplication_API.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> Search(string name);
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int Id);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<Employee> DeleteEmployee(int Id);
+    }
+}
